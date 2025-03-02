05d06be310b8ef4c30d742e0b2219c862c92a35e
Zohaib Ghazi <zohaib.ghazi@gmail.com>
2025-03-01 22:04:03 -0500

    Folder Level Changes

---
+++ Global/terraform-projects/02_Vault/variables.tf
@@ -0,0 +1,77 @@
+variable "proxmox_perf_host" {
+    description = "Proxmox Performance Node Name"
+    type = string
+    default = "pve"
+}
+
+variable "proxmox_nas_host" {
+    description = "Proxmox NAS Node Name"
+    type = string
+    default = "pve2"
+}
+variable "proxmox_api_perf_url" {
+    description = "PVE1 Performance Node Host Machine API URL"
+    type = string
+}
+
+variable "proxmox_api_stor_url" {
+    description = "PVE2 Storage Node Host Machine API URL"
+    type = string
+}
+
+variable "proxmox_api_token_id" {
+    description = "PVE Datacenter API Token ID for Terraform User"
+    type = string
+    sensitive = true
+}
+
+variable "proxmox_api_token_secret" {
+    description = "PVE Datacenter API Token Secret for Terraform User"
+    type = string
+    sensitive = true
+}
+
+variable "VM_DNS" {
+    description = "DNS Server IP(s)"
+    type = string
+    default = "1.1.1.1 8.8.8.8"
+}
+
+variable "ciuser" {
+    description = "Service Account - ciuser"
+    type = string
+    sensitive = true
+}
+
+variable "cipassword" {
+    description = "Service Account Password - ciuser"
+    type = string
+    sensitive = true
+}
+
+variable "sshkeys" {
+    description = "Service Account Password - ciuser"
+    type = string
+    sensitive = true
+}
+
+variable "ansible_user" {
+    description = "Ansible service account"
+    type = string
+}
+
+variable "private_key_path" {
+    description = "Private Key Path"
+    type = string
+    #should i add a sensitive=true here?
+}
+
+variable "public_key_path" {
+    description = "Public Key Path"
+    type = string
+}
+
+variable "directory" {
+    description = "Current Working Directory for Terraform & Ansible"
+    type = string
+}
